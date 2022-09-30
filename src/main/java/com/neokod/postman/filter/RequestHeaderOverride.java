21dd434025dee4980a9ee0ce971b48f258f6b67a
htemucin <htemucin@gmail.com>
2022-09-30 12:53:18 +0300

    header override is added

--- src/main/java/com/neokod/postman/filter/RequestHeaderOverride.java
+++ src/main/java/com/neokod/postman/filter/RequestHeaderOverride.java
@@ -14,24 +14,32 @@ public class RequestHeaderOverride implements PostmanItemFilter {
 
     @Override
     public void filter(PostmanItem postmanItem) {
-        if(CollectionUtils.isEmpty(requestProperties.getHeaderVariableList())) return;
-        if(postmanItem.getRequest() == null) return;
+        if (CollectionUtils.isEmpty(requestProperties.getHeaderVariableList())) return;
+        if (postmanItem.getRequest() == null) return;
         replaceHeaders(postmanItem.getRequest());
-        if(!CollectionUtils.isEmpty(postmanItem.getResponse())) {
-            for(PostmanResponse response : postmanItem.getResponse()) {
-                if(response.getOriginalRequest() != null)
+        if (!CollectionUtils.isEmpty(postmanItem.getResponse())) {
+            for (PostmanResponse response : postmanItem.getResponse()) {
+                if (response.getOriginalRequest() != null)
                     replaceHeaders(response.getOriginalRequest());
             }
         }
     }
 
     private void replaceHeaders(PostmanRequest request) {
-        if(!CollectionUtils.isEmpty(request.getHeader())) {
-            for(PostmanHeader header : request.getHeader()) {
-                for(PostmanVariable headerVariable : requestProperties.getHeaderVariableList()) {
-                    if(header.getKey().equalsIgnoreCase(headerVariable.getKey())) {
+        if (!CollectionUtils.isEmpty(request.getHeader())) {
+            boolean headerOverridden = false;
+            for (PostmanVariable headerVariable : requestProperties.getHeaderVariableList()) {
+                for (PostmanHeader header : request.getHeader()) {
+                    if (header.getKey().equalsIgnoreCase(headerVariable.getKey())) {
                         header.setValue(headerVariable.getValue());
+                        headerOverridden = true;
                     }
+                }
+                if (!headerOverridden) {
+                    PostmanHeader newHeader = new PostmanHeader();
+                    newHeader.setKey(headerVariable.getKey());
+                    newHeader.setValue(headerVariable.getValue());
+                    request.getHeader().add(newHeader);
                 }
             }
         }
